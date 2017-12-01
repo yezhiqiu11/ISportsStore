7916ddb6ef58c0d074b1b9aaeb9252cbd7725b2c
yezhiqiu <176017295@qq.com>
2017-12-01 17:00:56 +0800

    20171201 1700

--- ISportsStore/SportsStore.Domain/Abstract/IProductRepository.cs
+++ ISportsStore/SportsStore.Domain/Abstract/IProductRepository.cs
@@ -5,5 +5,6 @@ namespace SportsStore.Domain.Abstract
     public interface IProductRepository
     {
         IQueryable<Product> Products { get; }
+        void SaveProduct(Product product);
     }
 }
